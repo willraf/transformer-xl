35feee9e6faed5341a2dc5e35af4916889234bd1
Will Raftery <wr1g20@soton.ac.uk>
2024-05-16 17:31:43 +0100

    Change rem toeplitx funciton. Split heads into rsa and regular

--- pytorch/rem.py
+++ pytorch/rem.py
@@ -78,10 +78,11 @@ class REM(nn.Module):
     def compute_Ld(self, L, d):
         # Compute the indicator matrix: 1 where L is divisible by d, else 0
         indicator_matrix = (L % d == 0).int() 
+        indicator_matrix = indicator_matrix.to(dtype=torch.float32, device=self.device)
         
         # Compute the result matrix L_d by element-wise division where the indicator is 1
         L_d = (L / d) * indicator_matrix
-        
+        print(L_d.dtype)
         return L_d
 
 
