57912000561df8866338786d0f867f57c10d1964
Will Raftery <wr1g20@soton.ac.uk>
2024-05-16 22:54:55 +0100

    Change rem module

--- pytorch/rem.py
+++ pytorch/rem.py
@@ -63,26 +63,28 @@ class REM(nn.Module):
         L = self.create_Toeplitz_3D(self.d, self.truncation, query_len, key_len) # L is of shape (n_heads x query_len x key_len)
         
         k1, k2, k3, k4, k5, k6 = self.k1, self.k2, self.k3, self.k4, self.k5, self.k6
-        # L_non_dil = L[:k3, :, :]
-        # L_dil = L[k3:, :, :]
-        # result_L_dil = torch.empty_like(L_dil)
+        L_non_dil = L[:k3, :, :]
+        L_dil = L[k3:, :, :]
+        result_L_dil = torch.empty_like(L_dil)
 
         # k4 5 and 6 are diluted
         undil_n = k1+k2+k3
         dil_n = k4 + k5 + k6
         for i in range(undil_n, dil_n): 
-          L[i] = self.compute_Ld(L[i].clone(), self.d[i])
-          # result_L_dil[i] = self.compute_Ld(L_dil[i], self.d[i])
+          # L[i] = self.compute_Ld(L[i].clone(), self.d[i])
+          result_L_dil[i] = self.compute_Ld(L_dil[i], self.d[i])
+
+        L_new = torch.cat([L_non_dil, result_L_dil])
 
         # Rems 2 3 5 and 6 are cyclic
         s2,s3,s5,s6 = self.get_sinusoid(L,theta)
 
-        L1 = L[:k1]
-        L2 = L[k1:(k1+k2)]
-        L3 = L[(k1+k2):(k1+k2+k3)]
-        L4 = L[(k1+k2+k3):(k1+k2+k3+k4)]
-        L5 = L[(k1+k2+k3+k4):(k1+k2+k3+k4+k5)]
-        L6 = L[(k1+k2+k3+k4+k5):]
+        L1 = L_new[:k1]
+        L2 = L_new[k1:(k1+k2)]
+        L3 = L_new[(k1+k2):(k1+k2+k3)]
+        L4 = L_new[(k1+k2+k3):(k1+k2+k3+k4)]
+        L5 = L_new[(k1+k2+k3+k4):(k1+k2+k3+k4+k5)]
+        L6 = L_new[(k1+k2+k3+k4+k5):]
 
         # Regular (non cyclic) REMs
         L1 = pow(lambda_,L1)
