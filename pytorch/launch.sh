f47f48ac9eeecfefa2ead28ec486a7fb22990847
W.Raftery <wr1g20@cyan51.cluster.local>
2024-05-16 19:35:41 +0100

    Add launch.sh

---
+++ pytorch/launch.sh
@@ -0,0 +1,15 @@
+#!/bin/bash -l
+#SBATCH -p ecsstudents,ecsall
+#SBATCH --mem=32G
+#SBATCH --gres=gpu:1
+#SBATCH --nodes=1
+#SBATCH -c 32
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=wr1g20@soton.ac.uk
+#SBATCH --time=00:40:00
+
+module load conda/py3-latest
+conda activate transformer-xl
+
+bash run_enwik8_iridis.sh train --work_dir ~/xl-exp1
+
