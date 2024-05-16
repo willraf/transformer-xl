a7744cdf3f5414ee906c844cdfc16ef8e397f244
Will Raftery <wr1g20@soton.ac.uk>
2024-05-16 17:41:00 +0100

    Change rem toeplitx funciton. Split heads into rsa and regular

--- pytorch/mem_transformer.py
+++ pytorch/mem_transformer.py
@@ -548,7 +548,6 @@ class RSARelPartialLearnableMultiHeadAttn(RSARelMultiHeadAttn):
         rsa_attn_score, reg_attn_score = torch.split(attn_score, self.n_rsa_head, dim=-1)
 
         rems = self.rems(eta=self.eta, nu=self.nu, theta=self.theta, query_len=qlen, key_len=klen)
-        print('REMS', rems)
 
         # Repeat the rem for the number of batches. I think this is ok as the rem does not depend on the data in the batch
         rems = rems.unsqueeze(2)
@@ -556,7 +555,6 @@ class RSARelPartialLearnableMultiHeadAttn(RSARelMultiHeadAttn):
 
         # Get attention probs of rsa heads
         rsa_attn_prob = (1-F.sigmoid(self.mu)) * F.softmax(rsa_attn_score, dim=1) + F.sigmoid(self.mu) * rems
-        print('RSA attn prob', rsa_attn_prob)
 
         # [qlen x klen x bsz x n_head]
         # Get attention probs of regular heads
@@ -584,8 +582,6 @@ class RSARelPartialLearnableMultiHeadAttn(RSARelMultiHeadAttn):
             ##### residual connection + layer normalization
             output = self.layer_norm(w + attn_out)
 
-        print('Output')
-        print(output)
         return output
 
 class RelLearnableMultiHeadAttn(RelMultiHeadAttn):
