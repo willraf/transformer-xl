f1f8d2aff99645c34f8961e632bc3755bc495eba
Will Raftery <wr1g20@soton.ac.uk>
2024-05-16 22:37:30 +0100

    Change rem module

--- pytorch/rem.py
+++ pytorch/rem.py
@@ -17,7 +17,7 @@ class REM(nn.Module):
         self.k5 = k5
         self.k6 = k6
         
-        self.d = d
+        self.d = torch.tensor(d)
         self.truncation = truncation
 
         self.device = device
@@ -118,7 +118,7 @@ class REM(nn.Module):
         T = torch.tensor(toeplitz(y, x))
         T[T > 200] = 0
         L = T.unsqueeze(0).repeat(self.n_head, 1, 1)
-        return L
+        return L.to(dtype=torch.float32, device=self.device)
 
     # def create_Toeplitz_3D(self, d, truncation, query_len, key_len):
     #     x = np.arange(0, key_len)
@@ -143,8 +143,7 @@ class REM(nn.Module):
         # Compute the indicator matrix: 1 where L is divisible by d, else 0
         indicator_matrix = (L % d == 0).int() 
         indicator_matrix = indicator_matrix.to(dtype=torch.float32, device=self.device)
-        L = L.to(dtype=torch.float32, device=self.device)
-
+        # L = L.to(dtype=torch.float32, device=self.device)
         
         # Compute the result matrix L_d by element-wise division where the indicator is 1
         L_d = (L / d) * indicator_matrix
