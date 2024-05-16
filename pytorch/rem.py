35243c9e3dbf3fc1886b6ed92f86e1fce78ab4ec
Will Raftery <wr1g20@soton.ac.uk>
2024-05-16 22:29:17 +0100

    Change rem module

--- pytorch/rem.py
+++ pytorch/rem.py
@@ -22,7 +22,8 @@ class REM(nn.Module):
 
         self.device = device
         self.n_head = n_head
-        
+    
+    # # ORIGINAL
     # def get_sinusoid(self, L, theta):
 
     #     k1, k2, k3, k4, k5, k6 = self.k1, self.k2, self.k3, self.k4, self.k5, self.k6
@@ -34,32 +35,79 @@ class REM(nn.Module):
     #     s = torch.cat([s1,s2,s3,s4])
     #     return s
 
+    # WILLS
     def get_sinusoid(self, L, theta):
+
         k1, k2, k3, k4, k5, k6 = self.k1, self.k2, self.k3, self.k4, self.k5, self.k6
         M = L * theta
-        s1 = torch.cos(M[:k2, ])
-        s2 = torch.sin(M[k2:(k2+k3), ])
-        s3 = torch.cos(M[(k2+k3):(k2+k3+k5), ]) #!
-        s4 = torch.sin(M[(k2+k3+k5):(k2+k3+k5+k6), ]) #!
-        s = torch.cat([s1,s2,s3,s4])
-        return s.to(dtype=torch.float32, device=self.device)
+        s2 = torch.cos(M[k1:(k1+k2), ])
+        s3 = torch.sin(M[k2:(k2+k3), ])
+        s5 = torch.cos(M[(k2+k3+k4):(k2+k3+k4+k5), ])
+        s6 = torch.sin(M[(k2+k3+k4+k5):, ])
+        return s2,s3,s5,s6
+
+    # # RORYS
+    # def get_sinusoid(self, L, theta):
+    #     k1, k2, k3, k4, k5, k6 = self.k1, self.k2, self.k3, self.k4, self.k5, self.k6
+    #     M = L * theta
+    #     s1 = torch.cos(M[:k2, ])
+    #     s2 = torch.sin(M[k2:(k2+k3), ])
+    #     s3 = torch.cos(M[(k2+k3):(k2+k3+k5), ]) #!
+    #     s4 = torch.sin(M[(k2+k3+k5):(k2+k3+k5+k6), ]) #!
+    #     s = torch.cat([s1,s2,s3,s4])
+    #     return s.to(dtype=torch.float32, device=self.device)
 
     def forward(self, eta, nu, theta, query_len, key_len):
         lambda_ = torch.tanh(eta)
         gamma = torch.sigmoid(nu)
         L = self.create_Toeplitz_3D(self.d, self.truncation, query_len, key_len) # L is of shape (n_heads x query_len x key_len)
-        # L1 = L[:2, :, :]
-        # L2 = L[2:, :, :]
-        # # s1,s2,s3,s4 = get_sinusoid(L,theta)
-        # s = self.get_sinusoid(L2, theta)
+        
+        k1, k2, k3, k4, k5, k6 = self.k1, self.k2, self.k3, self.k4, self.k5, self.k6
+        # L_non_dil = L[:k3, :, :]
+        # L_dil = L[k3:, :, :]
+
+        # result_L_dil = torch.empty_like(L_dil)
+        # k4 5 and 6 are diluted
+        undil_n = k1+k2+k3
+        dil_n = k4 + k5 + k6
+        for i in range(undil_n, dil_n): 
+          L[i] = self.compute_Ld(L[i], self.d[i])
+
+        L = L.to(dtype=torch.float32, device=self.device)
+
+        # Rems 2 3 5 and 6 are cyclic
+        s2,s3,s5,s6 = get_sinusoid(L,theta)
+
+        L1 = L[:k1]
+        L2 = L[k1:(k1+k2)]
+        L3 = L[(k1+k2):(k1+k2+k3)]
+        L4 = L[(k1+k2+k3):(k1+k2+k3+k4)]
+        L5 = L[(k1+k2+k3+k4):(k1+k2+k3+k4+k5)]
+        L6 = L[(k1+k2+k3+k4+k5):]
+
+        # Regular (non cyclic) REMs
+        L1 = pow(lambda_,L1)
+        L4 = pow(lambda_,L4)
+
+        # Cyclic REMs
+        L2 = pow(gamma,L2) * s2
+        L3 = pow(gamma,L3) * s3
+        L5 = pow(gamma,L5) * s5
+        L6 = pow(gamma,L6) * s6
+
+        REM = torch.cat([L1, L2, L3, L4, L5, L6])
+
+        # s = self.get_sinusoid(L, theta)
         # powered_lambda = pow(lambda_,L1)
         # powered_gamma = pow(gamma,L2)
         # REM = torch.cat([powered_lambda, (powered_gamma * s)])
 
+
+
         # s1,s2,s3,s4 = get_sinusoid(L,theta)
-        s = self.get_sinusoid(L, theta)
-        powered_gamma = pow(gamma,L)
-        REM = powered_gamma * s   
+        # s = self.get_sinusoid(L, theta)
+        # powered_gamma = pow(gamma,L)
+        # REM = powered_gamma * s   
 
         return REM.permute(1, 2, 0)      # query_len x key_len x n_heads
 
@@ -70,16 +118,25 @@ class REM(nn.Module):
         T = torch.tensor(toeplitz(y, x))
         T[T > 200] = 0
         L = T.unsqueeze(0).repeat(self.n_head, 1, 1)
+        return L
 
-        d = torch.tensor(d).view(self.n_head, 1, 1)
-        # d = d.to(dtype=torch.float32, device=self.device)
-        # L = L.to(dtype=torch.float32, device=self.device)
+    # def create_Toeplitz_3D(self, d, truncation, query_len, key_len):
+    #     x = np.arange(0, key_len)
+    #     y = np.arange(0, query_len)
 
-        result_tensor = torch.empty_like(L)
-        # Apply the function to each L matrix with the corresponding d value
-        for i in range(self.n_head):
-            result_tensor[i] = self.compute_Ld(T[i], d[i])
-        return result_tensor.to(dtype=torch.float32, device=self.device)
+    #     T = torch.tensor(toeplitz(y, x))
+    #     T[T > 200] = 0
+    #     L = T.unsqueeze(0).repeat(self.n_head, 1, 1)
+
+    #     d = torch.tensor(d).view(self.n_head, 1, 1)
+    #     # d = d.to(dtype=torch.float32, device=self.device)
+    #     # L = L.to(dtype=torch.float32, device=self.device)
+
+    #     result_tensor = torch.empty_like(L)
+    #     # Apply the function to each L matrix with the corresponding d value
+    #     for i in range(self.n_head):
+    #         result_tensor[i] = self.compute_Ld(T[i], d[i])
+    #     return result_tensor.to(dtype=torch.float32, device=self.device)
 
 
     def compute_Ld(self, L, d):
@@ -93,84 +150,3 @@ class REM(nn.Module):
         # Compute the result matrix L_d by element-wise division where the indicator is 1
         L_d = (L / d) * indicator_matrix
         return L_d
-
-
-# import numpy as np
-# import torch
-# from scipy.linalg import toeplitz
-# import torch.nn as nn
-
-
-# class REM(nn.Module):
-#     # initialise the object
-#     def __init__(self, k1, k2, k3, k4, k5, k6, d, truncation, device, n_head):
-#         super(REM, self).__init__()
-        
-#         self.k1 = k1 #(reg)
-#         self.k2 = k2 #(c1)
-#         self.k3 = k3 #(c2)
-#         # dilated versions
-#         self.k4 = k4
-#         self.k5 = k5
-#         self.k6 = k6
-        
-#         self.d = d
-#         self.truncation = truncation
-        
-#         self.device = device
-        
-  
-#     def get_sinusoid(self, L, theta):
-#         M = L * theta
-#         s1 = torch.cos(M[:self.k2, ]).to(self.device) 
-#         s2 = torch.sin(M[self.k2:(self.k2+self.k3), ]).to(self.device) 
-#         s3 = torch.cos(M[(self.k2+self.k3):(self.k2+self.k3+self.k5), ]) #!
-#         s4 = torch.sin(M[(self.k2+self.k3+self.k5):(self.k2+self.k3+self.k5+self.k6), ]) #!
-#         s = torch.cat([s1,s2,s3,s4]).to(self.device)
-#         return s
-        
-#     def forward(self, eta, nu, theta, query_len, key_len):
-#         # print(f'k1: {self.k1}, k2: {self.k2}, k3: {self.k3}, k4: {self.k4}, k5: {self.k5}, k6: {self.k6} ')
-#         lambda_ = torch.tanh(eta).to(self.device)
-#         gamma = torch.sigmoid(nu).to(self.device)
-#         L = self.create_Toeplitz_3D(self.d, self.truncation, query_len, key_len) # L is of shape (n_heads x query_len x key_len)
-        
-#         powered_lambda = pow(lambda_,L)
-#         powered_gamma = pow(gamma, L) 
-#         s = self.get_sinusoid(L, theta) #cyclics
-        
-#         powered_gamma = powered_gamma[:self.k2 + self.k3 + self.k5 + self.k6] #!
-#         powered_lambda = powered_lambda[:self.k1 + self.k4] #!
-        
-#         # initialize with just the regulars and then add the dilated
-#         regular_rems = powered_lambda[:self.k1]
-#         cyclic_rems = powered_gamma[:self.k2 + self.k3]
-                
-#         # dilate regular rems: (k4)
-#         n_dilated_regs = self.k4
-#         for i in range(n_dilated_regs):
-#             dilated_reg_rem = torch.kron(powered_lambda[self.k1 + i], torch.eye(n=self.d.pop()).to(self.device)).to(self.device)
-#             dilated_reg_rem = dilated_reg_rem[:L.shape[1], :L.shape[2]]
-#             regular_rems = torch.cat([regular_rems, torch.unsqueeze(dilated_reg_rem, 0).to(self.device)]).to(self.device)
-
-#         # dilate cyclic rems: (k5, k6)
-#         n_dilated_cyclics = self.k5 + self.k6
-#         for j in range(n_dilated_cyclics):
-#             dilated_cyclic_rem = torch.kron(s[self.k2+self.k3+j], torch.eye(n=self.d.pop()).to(self.device)).to(self.device)
-#             dilated_cyclic_rem = dilated_cyclic_rem[:L.shape[1], :L.shape[2]]        
-#             cyclic_rems = torch.cat([cyclic_rems, torch.unsqueeze(dilated_cyclic_rem, 0).to(self.device)]).to(self.device)
-        
-#         # mask & -I
-#         REM = torch.cat([regular_rems, (cyclic_rems * s)]).to(self.device)
-#         REM = torch.tril(REM).to(self.device) - torch.eye(n=REM.shape[1], m=REM.shape[2]).to(self.device)
-#         return REM
-
-#     def create_Toeplitz_3D(self, d, truncation, query_len, key_len):
-#         x = np.arange(0, key_len)
-#         y = np.arange(0, query_len)
-
-#         A = toeplitz(y, x)
-#         A[A > 200] = 0
-#         L = torch.from_numpy(A).to(self.device)
-#         L = torch.stack([L]*4, 0).to(self.device)
-#         return L.to(dtype=torch.float32, device=self.device)
